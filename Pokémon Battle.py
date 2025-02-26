6ef96cc0b58da45677c4847375bfa1a1668593c5
python-processing-unit <jakedan09@gmail.com>
2025-02-26 14:43:48 -0500

    Fixed STAB bug
    STAB didn't apply if only the Pokemon's second type matched the attack type

--- Pokémon Battle.py
+++ Pokémon Battle.py
@@ -7,9 +7,10 @@ import trainers
 import type_matrix as ty
 path = os.path.abspath(sys.argv[0])
 path = path.replace("Pokémon Battle.py","")
-def effective(e,t1,t2=None): # e attacking t1 and t2 (if present)
-    list_index1 = -1 # Initialize with a default value
-    list_index2 = -1 # Initialize with a default value
+#Calculate effectiveness of one type attacking another
+def effective(e,t1,t2=None):#Attack Type, Defending Pokemon Type 1, Defending Pokemon Type 2
+    list_index1 = -1
+    list_index2 = -1
     if(isinstance(t1, int)):
         list_index1 = t1
     else:
@@ -30,33 +31,7 @@ def effective(e,t1,t2=None): # e attacking t1 and t2 (if present)
         effectiveness *= e[list_index2]
     return effectiveness
 na = None
-def calc_stat(BaseStat):
-    return(((((BaseStat+15)*2+31.5)*50)/100)+5)
-def calc_health(BaseStat):
-    return(((((BaseStat+15)*2+31.5)*50)/100)+60)
-class pokemon():
-    def __init__(self,hp,atk,dfn,spe,spd,atk1,atk2,atk3,atk4,nm,t1,t2=None):
-        self.attack = round(calc_stat(atk),0)
-        self.defense = round(calc_stat(dfn),0)
-        self.speed = round(calc_stat(spd),0)
-        self.special = round(calc_stat(spe),0)
-        self.hp = round(calc_health(hp),0)
-        self.max_hp = self.hp
-        self.stats = [self.attack,self.defense,self.speed,self.special,self.hp]
-        self.move1 = atk1
-        self.move2 = atk2
-        self.move3 = atk3
-        self.move4 = atk4
-        self.type1 = t1 if isinstance(t1,int) else ty.types.index(t1)
-        self.type2 = t2 if isinstance(t2,int) else(ty.types.index(t2) if t2 else None)
-        self.name = str(nm)
-    def handle_attack(self,damage):
-        if(damage >= 0):
-            self.hp = self.hp-round(damage,0)
-            if(self.hp<0):
-                self.hp = 0
-        else:
-            print("Cannot apply negative damage")
+#LCG PRNG from Pokémon Gen-I
 class LCG:
     def __init__(self, seed):
         self.a = 1103515245
@@ -68,6 +43,7 @@ class LCG:
         return self.seed
     def next(self):
         return self.next_32() & 0xFF
+#User inputs attack choice
 def get_attack(poke):
     while(True):
         try:
@@ -79,14 +55,15 @@ def get_attack(poke):
             return(poke.move_list[int(attk)-1])
         except:
             print("Invalid input")
-def physical_attack(pokea,poked,attack_name):
+#Code for a physical attack
+def physical_attack(pokea,poked,attack_name):#Attacking Pokemon, Defending Pokemon, Attack
     try:
         damage = (((14.285714285714286*attack_name.power*(pokea.attack/poked.defense))/50)+2)
         try:
             damage = damage * effective(attack_name.move_type,poked.type1,poked.type2)
         except:
             damage = damage * effective(attack_name.move_type,poked.type1)
-        if((attack_name.move_type == pokea.type1) or (attack_name.move_type==pokea.type1)):
+        if((attack_name.move_type == pokea.type1) or (attack_name.move_type==pokea.type2)):
             damage = damage * 1.5
         poked.handle_attack(damage)
         print(pokea.name+" used "+attack_name.name)
@@ -102,7 +79,8 @@ def physical_attack(pokea,poked,attack_name):
         print("But it did nothing")
         print(pokea.name+"'s HP: "+str(int(pokea.hp))+"/"+str(int(pokea.max_hp)))
         print(poked.name+"'s HP: "+str(int(poked.hp))+"/"+str(int(poked.max_hp)))
-def special_attack(pokea,poked,attack_name):
+#Code for a special attack
+def special_attack(pokea,poked,attack_name):#Attacking Pokemon, Defending Pokemon, Attack
     damage = (((14.285714285714286*attack_name.power*(pokea.special/poked.special))/50)+2)
     try:
         damage = damage * effective(attack_name.move_type,poked.type1,poked.type2)
@@ -121,7 +99,7 @@ def special_attack(pokea,poked,attack_name):
     print("Did "+str(int(damage))+" damage to "+poked.name)
     print(pokea.name+"'s HP: "+str(int(pokea.hp))+"/"+str(int(pokea.max_hp)))
     print(poked.name+"'s HP: "+str(int(poked.hp))+"/"+str(int(poked.max_hp)))
-def attack(pokea,poked,attack_name):
+def attack(pokea,poked,attack_name):#Attacking Pokemon, Defending Pokemon, Attack
     ran = lcg.next()
     if(attack_name == None):
         pass
@@ -136,18 +114,42 @@ def attack(pokea,poked,attack_name):
             print("But it missed")
             print(pokea.name+"'s HP: "+str(int(pokea.hp))+"/"+str(int(pokea.max_hp)))
             print(poked.name+"'s HP: "+str(int(poked.hp))+"/"+str(int(poked.max_hp)))
+def opponent_attack1():
+    try:
+        attack(opponent,active,opponent.move1)
+    except:
+        print(opponent.name+" used "+opponent.move1.name+", but it did nothing")
+def opponent_attack2():
+    try:
+        attack(opponent,active,opponent.move1)
+    except:
+        print(opponent.name+" used "+opponent.move1.name+", but it did nothing")
+def opponent_attack3():
+    try:
+        attack(opponent,active,opponent.move1)
+    except:
+        print(opponent.name+" used "+opponent.move1.name+", but it did nothing")
+def opponent_attack4():
+    try:
+        attack(opponent,active,opponent.move1)
+    except:
+        print(opponent.name+" used "+opponent.move1.name+", but it did nothing")
+#Opponent Logic
 def opponent_attack():
     atk_select = random.randint(1,4)
     if(atk_select == 1):
-        attack(opponent,active,opponent.move1)
+        opponent_attack1()
     elif(atk_select == 2):
-        attack(opponent,active,opponent.move2)
+        opponent_attack2()
     elif(atk_select == 3):
-        attack(opponent,active,opponent.move3)
+        opponent_attack3()
     else:
-        attack(opponent,active,opponent.move4)
+        opponent_attack4()
+#Code to launch a battle, and the battle logic
 def battle():
     global active
+    print(active.name+"'s HP: "+str(int(active.hp))+"/"+str(int(active.max_hp)))
+    print(opponent.name+"'s HP: "+str(int(opponent.hp))+"/"+str(int(opponent.max_hp)))
     while((active.hp > 0) and (opponent.hp > 0)):
         print("Attack (1)")
         print("Switch (2)")
@@ -179,10 +181,8 @@ def battle():
                 except:
                     print("Invalid input")
 lcg = LCG(seed=random.randint(0,99999))
-pkmn1, pkmn2, pkmn3 = trainers.red()
-pokemon_party = [pkmn1,pkmn2,pkmn3]
+pokemon_party = trainers.red()
 active = pokemon_party[0]
-opkmn1, opkmn2, opkmn3 = trainers.blue()
-opponent_party = [opkmn1,opkmn2,opkmn3]
-opponent = opkmn1#opponent_party[0]
+opponent_party = trainers.red()
+opponent = opponent_party[0]
 battle()
